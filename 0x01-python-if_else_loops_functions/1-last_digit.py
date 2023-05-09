d18b27c41e558d7c5e1d061349ef362748525f91
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-05-09 02:07:27 -0700

    remove convert negative to positive

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,8 +4,6 @@ number = random.randint(-10000, 10000)
 num = number / 10
 temp = int(num) * 10
 res = number - temp
-if (res < 0):
-    res *= -1
 if (res > 5):
     print(f"Last digit of {number} is {res} and is greater than 5")
 elif (res == 0):
