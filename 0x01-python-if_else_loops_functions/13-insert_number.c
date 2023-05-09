68930fbae82b649e8feae5ce6996484650595523
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-05-09 10:29:58 -0700

    function that insert node of int in position of sorted linked list

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,49 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * insert_node - inset node to list accord
+ * to it's number to be sorted
+ * @head: pointer to pointer of list
+ * @number: number inserted to list
+ * Return: pointer to new node or NULL
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *node, *ptr = *head, *temp;
+
+	node = malloc(sizeof(listint_t));
+	if (node == NULL)
+		return (NULL);
+	node->n = number;
+	if (*head == NULL)
+	{
+		node->next = NULL;
+		*head = node;
+	}
+	while (ptr != NULL)
+	{
+		temp = ptr->next;
+		if (number > ptr->n && number < temp->n)
+		{
+			node->next = temp;
+			ptr->next = node;
+			break;
+		}
+		else if (number > ptr->n && temp == NULL)
+		{
+			ptr->next = node;
+			node->next = NULL;
+			break;
+		}
+		else if (number < ptr->n)
+		{
+			node->next = *head;
+			*head = node;
+			break;
+		}
+		ptr = temp;
+	}
+	return (node);
+}
