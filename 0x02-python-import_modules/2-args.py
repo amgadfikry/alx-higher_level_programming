deb454dc4af02ab2cf201c4fd13dfd9f29f676b9
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-05-11 02:14:44 -0700

    add condition to write argumen instead to arguments if only one

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,7 +7,7 @@ if (__name__ == "__main__"):
         if (len(sys.argv) == 2):
             print(f"1 argument:")
         else:
-            print(f"{len(sys.argv)} arguments:")
+            print(f"{len(sys.argv) - 1} arguments:")
         i = 0
         for arg in sys.argv:
             if (i == 0):
