5ba8402b93606b49dba7753c37bc622a1029e50c
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-05-12 21:00:41 -0700

    if bot contaon list

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix is None:
-        return None
     for num in matrix:
-        for n in num:
-            if (n == num[len(num) - 1]):
-                print("{:d}".format(n))
+        if type(num) is list:
+            for n in num:
+                if (n == num[len(num) - 1]):
+                    print("{:d}".format(n))
+                else:
+                    print("{:d}".format(n), end=" ")
+        else:
+            if (num == matrix[len(matrix) - 1]):
+                print("{:d}".format(nun))
             else:
-                print("{:d}".format(n), end=" ")
+                print("{:d}".format(num), end=" ")
