e9eb635683c7eba46e18ab4bfb22a55e2fe4b9b5
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-05-12 20:47:03 -0700

    fix function

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    li = my_list[:]
     if (idx < 0 or idx > len(my_list)):
-        return my_list[:]
-    return my_list[:][idx] = element
+        return li
+    li[idx] = element
+    return li
     
