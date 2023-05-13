11deea534adfcc0de5cd50d8113a70a5124c621f
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-05-12 21:09:53 -0700

    return function to first solution

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for num in matrix:
-        if type(num) is list:
-            for n in num:
-                if (n == num[len(num) - 1]):
-                    print("{:d}".format(n))
-                else:
-                    print("{:d}".format(n), end=" ")
-        else:
-            if (num == matrix[len(matrix) - 1]):
-                print("{:d}".format(nun))
+        for n in range(len(num):
+            if (n == len(num) - 1):
+                print("{:d}".format(num[n]))
             else:
-                print("{:d}".format(num), end=" ")
+                print("{:d}".format(num[n]), end=" ")
