5bdae7c8f1865a19bd93235d2e1500304bb417fa
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-05-30 13:39:13 -0700

    add exception print line

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
-
-"""python script to training on classes and oop"""
-
-
 class Square:
-    """Class Square that has attributes. Instantiation with size"""
+    """Class Square that has attributes. Instantiation with size
+
+    Attributes:
+        size (int): The size of the square
+    """
+
     def __init__(self, size=0, position=(0, 0)):
         """The __init__ method for Square class
+
         Args:
-             A private instance size
-             A private intance position
+            size: (:obj: 'int', optional): A public instance size
+            position: (:obj: 'tuple', optional): A public instance position
         """
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
-        """getter function to get size
+        """Call the function to checking property
+
         Returns:
             The size of the square
         """
@@ -24,17 +27,20 @@ class Square:
 
     @property
     def position(self):
-        """ getter of position attribute
-            Returns:
-                value of position
+        """Call the function to checking property
+
+        Returns:
+            The tuple position
         """
         return self.__position
 
     @size.setter
     def size(self, value):
         """check errors and setter for size attribute
+
         Args:
             value: Value to checking errors
+
         Raises:
             TypeError: Exception if size is not an integer
             ValueError: Exception if size is less than 0
@@ -48,11 +54,13 @@ class Square:
 
     @position.setter
     def position(self, value):
-        """setter of position attribute
-            Args:
-                value: value of position
-            Raises:
-                TypeError:if type is no tuple or values is positive
+        """check errors and setter for size attribute
+
+        Args:
+            value: Value to checking errors
+
+        Raises:
+            TypeError: Exception if size is not an integer
         """
         if type(value) is not tuple or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
@@ -64,21 +72,24 @@ class Square:
 
     def area(self):
         """Calculates the area of the square
+
         Returns:
             The area of the square
         """
         return self.__size ** 2
 
     def my_print(self):
-        """function that print # accord to square area"""
+        """Print a square using # character
+        """
+
         if self.__size == 0:
-            print("")
+            print()
         else:
-            for z in range(self.position[1]):
-                print("")
+            for i in range(self.position[1]):
+                print()
             for i in range(self.size):
-                for x in range(self.position[0]):
-                    print(" ", end="")
-                for j in range(self.size):
+                for j in range(self.position[0]):
+                    print(end=" ")
+                for k in range(self.size):
                     print("#", end="")
-                print("")
+                print()
