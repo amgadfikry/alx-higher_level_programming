e46d7173daebe1f9e88d2a888e9fcfb171e3b90a
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-05-30 00:47:54 -0700

    addmethod that print # accord to area of square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+class Square:
+    """square class without any public attributes"""
+    def __init__(self, size=0):
+        """init methode that start instance with attribute
+            size for everyone
+            Args:
+                size: is size of square may be any data type
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """getter methode to get size"""
+        return self.__size
+
+    @size.setter
+    def size(self, num):
+        """ setter for size of square and check if digit or not
+            and less of 0 or not
+            Args:
+                num: number to add to size
+            """
+        if not isinstance(num, int):
+            raise ValueError("size must be an intger")
+        elif num < 0:
+            raise TypeError("size must be >= 0")
+        else:
+            self.__size = num
+
+    def area(self):
+        """ function that calculate area of square
+            Returns: square of size
+        """
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """function that print # accord to square area"""
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(self.__size):
+                for i in range(self.__size):
+                    print("#", end="")
+                print("")
