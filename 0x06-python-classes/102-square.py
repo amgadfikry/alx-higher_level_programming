b708897d91ef07423b010c944a12b4b9d710e896
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-05-30 19:26:45 -0700

    add comparision status between two instance in class

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python3
+
+"""python script to training on classes and oop"""
+
+
+class Square:
+    """Class Square that has attributes. Instantiation with size"""
+    def __init__(self, size=0):
+        """The __init__ method for Square class
+        Args:
+             A private instance size
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """getter function to get size
+        Returns:
+            The size of the square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """check errors and setter for size attribute
+        Args:
+            value: Value to checking errors
+        Raises:
+            TypeError: Exception if size is not an integer
+            ValueError: Exception if size is less than 0
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Calculates the area of the square
+        Returns:
+            The area of the square
+        """
+        return self.__size ** 2
+
+    def __eq__(self, second):
+        """calculate equality of two instance
+            Return:
+                true or false
+        """
+        return self.area() == second.area()
+
+    def __ne__(self, second):
+        """calculate none equality of two instance
+            Return:
+                true or false
+        """
+        return self.area() != second.area()
+
+    def __gt__(self, second):
+        """calculate if greater than second instance
+            Return:
+                true or false
+        """
+        return self.area() > second.area()
+
+    def __ge__(self, second):
+        """calculate if greater than or equal of second instance
+            Return:
+                true or false
+        """
+        return self.area() >= second.area()
+
+    def __lt__(self, second):
+        """calculate if lesser than second instance
+            Return:
+                true or false
+        """
+        return self.area() < second.area()
+
+    def __le__(self, second):
+        """calculate if lesser than or equal of second instance
+            Return:
+                true or false
+        """
+        return self.area() <= second.area()
