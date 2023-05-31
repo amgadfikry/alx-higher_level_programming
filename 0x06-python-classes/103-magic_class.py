41ba0e17d4dab5ff2bd4f5da37dc8865cbdd1259
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-05-30 19:43:08 -0700

    python class from bytecode python

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" magic class that calculate area  and circumference"""
+
+
+class MagicClass:
+    """ class of magic that can make instance from it"""
+
+    def __init__(self, radius):
+        """init of class attr
+            check if radius is int or float
+            Attr:
+                radius: private readius number
+        """
+        if type(radius) is not int or type(radius) is not float:
+            raise TypeError("radius must be a number")
+        else:
+            self.__radius = radius
+
+    def area(self):
+        """ calculate area of instance
+            Returns:
+                area value
+        """
+        return self.__radius ** 2
+
+    def circumference(self):
+        """calculate circumference by radius
+            Return:
+                value of circumference
+        """
+        return 2 ** self.__radius
