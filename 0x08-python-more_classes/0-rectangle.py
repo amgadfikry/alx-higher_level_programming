397cd7a7fd28af4b119d1af2d40064e99f8deaea
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-06-04 23:53:11 -0700

    empty class that define rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""module that define rectangle class with full attributes
+    with static, class and instance methods
+"""
+
+
+class Rectangle:
+    """Rectangle class with it's methods and attributes"""
+    pass
