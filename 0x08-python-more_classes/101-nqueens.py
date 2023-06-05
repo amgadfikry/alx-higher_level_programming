3854e0f7c67d1d11173f83ac4dcdf822e36b4894
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-06-05 02:47:46 -0700

    script with except condition only not complete solution

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -6,10 +6,15 @@ if len(sys.argv) != 2:
     print("Usage: nqueens N")
     sys.exit(1)
 
-if type(int(sys.argv[1])) is not int:
+try:
+    n = int(sys.argv[1])
+except ValueError:
+    n = "not"
+
+if type(n) is not int:
     print("N must be a number")
     sys.exit(1)
 
-if int(sys.argv[1]) < 4:
+if n < 4:
     print("N must be at least 4")
     sys.exit(1)
