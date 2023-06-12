3b2792ed9a2f63e6e840b61c4da7a113b9683674
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-06-12 09:02:04 -0700

    create str methode

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" module of subclass of rectangle"""
+p_cls = __import__("9-rectangle").Rectangle
+
+
+class Square(p_cls):
+    """class of square inherited from recatangle class
+        which inherit from basegeometry
+    """
+
+    def __init__(self, size):
+        """init method that start attribute
+            Attr:
+                size: size of square
+        """
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """ magic method that return string represent class
+        """
+        return f"[Square] {self.__size}/{self.__size}"
