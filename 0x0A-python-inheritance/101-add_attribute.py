3e37ac68248c9c7e237295f156ba62925b0805a4
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-06-12 13:34:13 -0700

    create function add new attribute to object

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -5,7 +5,7 @@
 def add_attribute(obj, att, value):
     """ function to set attributes"""
 
-    if type(obj) in [int, str, float, set]:
+    if type(obj) in [int, str, float, bool, complex]:
         raise TypeError("can't add new attribute")
     else:
         setattr(obj, att, value)
