a2f5604dd41b1da672c39f170d0b16f6e421231d
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-06-13 13:27:07 -0700

    add exception of ztrl + c

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -18,16 +18,22 @@ dic = {
 }
 row = []
 i = 0
-for line in sys.stdin:
-    li = line.split(" ")
-    size += int(li[-1])
-    if li[-2] in dic:
-        dic[li[-2]] += 1
-        if li[-2] not in row:
-            row.append(li[-2])
-    i += 1
-    if i == 10:
-        print(f"File size: {size}")
-        for x in sorted(row):
-            print(f"{x}: {dic[x]}")
-        i = 0
+try:
+    for line in sys.stdin:
+        li = line.split(" ")
+        size += int(li[-1])
+        if li[-2] in dic:
+            dic[li[-2]] += 1
+            if li[-2] not in row:
+                row.append(li[-2])
+        i += 1
+        if i == 10:
+            print(f"File size: {size}")
+            for x in sorted(row):
+                print(f"{x}: {dic[x]}")
+            i = 0
+except KeyboardInterrupt:
+    print(f"File size: {size}")
+    for x in sorted(row):
+        print(f"{x}: {dic[x]}")
+    raise
