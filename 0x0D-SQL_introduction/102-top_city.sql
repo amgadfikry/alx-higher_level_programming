56c8384556dea4c0a62adf4af435a00e146c407f
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-07-18 16:10:07 -0700

    script get average of temperature in specific months

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -2,6 +2,6 @@
 -- statement get average of temperature in specific months
 SELECT city, AVG(value) AS avg_temp FROM temperatures
 	GROUP BY city
-	HAVING month = 7 AND month = 8
+	HAVING month = 7 OR month = 8
 	ORDER BY avg_temp DESC
 	LIMIT 3;
