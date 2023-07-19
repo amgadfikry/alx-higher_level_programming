6fb505c329a3565bd2a2b729bd56eab45341945b
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-07-19 08:27:45 -0700

    list all rating sum of show

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,6 @@
 -- list all shows by rating
 -- list all show by rating
-SELECT tv_shows.title, AVG(tv_show_ratings.rate) AS rating
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
 FROM tv_shows
 INNER JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
 GROUP BY tv_shows.title
