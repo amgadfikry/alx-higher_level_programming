5887dec54e7a974f0ca981fa4eb22752c66fb79c
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-07-19 02:17:56 -0700

    order list

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- list all citiesof california
 -- list all cities without join
-SELECT * FROM cities WHERE state_id = (
+SELECT id, name FROM cities WHERE state_id = (
 	SELECT id FROM states WHERE name = 'California')
 	ORDER BY cities.id ASC;
