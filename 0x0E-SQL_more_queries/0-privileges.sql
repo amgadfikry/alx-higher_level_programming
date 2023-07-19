ac9ce8eff8e6b74a72c156be577395b3c82f5ea8
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-07-19 02:12:21 -0700

    fix comment for other statement

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,5 @@
 -- script show privilages
 -- statement show all privilage of useres
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- show other user
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 -- create new user in mysql
 -- create new user in mysql
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+-- allowprivilages
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
