9a10834b61dd8c1c06bd8bc052c567e1fc36387b
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-07-19 08:44:12 -0700

    list all genres with there rating sum

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -2,7 +2,8 @@
 -- list genre by there rating
 SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
 FROM tv_genres
-INNER JOIN tv_show_genres ON tv_shows.id = tv_show_generes.genre_id
-AND tv_show_genres.show_id = tv_show_ratings.show_id
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_generes.genre_id
+INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+INNER JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
 GROUP BY tv_genres.name
 ORDER BY rating DESC;
