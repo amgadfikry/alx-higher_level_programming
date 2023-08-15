46d3d29225831ed8d9bd188ce9a7a57e617f9abd
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-08-14 23:09:42 -0700

    modified rectabgle print methd

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -8,11 +8,11 @@ class Rectangle {
     }
   }
 
-  print () {
+  print (c = 'X') {
     for (let i = 0; i < this.height; i++) {
       let x = '';
       for (let j = 0; j < this.width; j++) {
-        x += 'X';
+        x += c;
       }
       console.log(x);
     }
