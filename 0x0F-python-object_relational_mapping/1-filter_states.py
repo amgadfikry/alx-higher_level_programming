368792e9a02ca529097cd108abc7688f2dc5037d
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-08-17 16:51:20 -0700

    create python script get states from satabase filtered with start with letter 'N'

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" module to get all state from dtatbases with filter """
+import MySQLdb
+import sys
+
+
+def main():
+    """mian function not excuted when import as module"""
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1], port=3306,
+                         passwd=sys.argv[2], db=sys.argv[3])
+    start = db.cursor()
+    start.execute("""SELECT * FROM states WHERE states.name LIKE 'N%'
+                        ORDER BY states.id""")
+    rows = start.fetchall()
+    for row in rows:
+        print(row)
+
+
+if __name__ == "__main__":
+    main()
