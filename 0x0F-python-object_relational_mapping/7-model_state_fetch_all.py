2d66b380e1c60462b6d359e09cfac9da04c14db5
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-08-18 07:32:22 -0700

    craete script get all states from databases using ORM

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""module that retrive states from database"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+from model_state import State, Base
+from sys import argv
+
+
+engine = create_engine(f"mysql+mysqldb://{argv[1]}:{argv[2]}\
+                       @localhost:3306/{argv[3]}")
+Session = sessionmaker(bind=engine)
+session = Session()
+rows = session.query(State).order_by(State.id).all()
+for row in rows:
+    print(f"{row.id}: {row.name}")
