83b1c9ba3156345b288f9750dd5d4531ee59faed
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-09-01 15:28:12 -0700

    fetch data through package requests

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" script fetch data using request package """
+from requests import get
+
+
+def main():
+    """ function make file not excute if it is not imported """
+    res = get("https://alx-intranet.hbtn.io/status")
+    print("""Body response:
+\t- type: {}
+\t- content: {}""".format(type(res.text), res.text)
+)
+
+if __name__ == "__main__":
+    main()
