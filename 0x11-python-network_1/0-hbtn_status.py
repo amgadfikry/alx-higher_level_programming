587dc4c5f7e7b3e92dd5d43b6032359b766a01b5
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-09-01 14:30:35 -0700

    python script fetch url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,8 +7,15 @@ def main():
     """ function make file not excute if it is not imported """
     url = "https://alx-intranet.hbtn.io/status"
     with urllib.request.urlopen(url) as res:
-        body = res.read().decode("utf-8")
-        print(body)
+        body = res.read()
+        print("""Body response:
+            - type: {}
+            - content: {}
+            - utf8 content: {}""".format(
+                type(body),
+                body,
+                body.decode("utf-8"))
+            )
 
 
 if __name__ == "__main__":
