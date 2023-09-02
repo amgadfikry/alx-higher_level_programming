87d99fb3cc02684ea08830e74fd1e6d5fcb146e8
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-09-01 17:34:36 -0700

    script get last ten commit

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" script that git last 5 commits"""
+from requests import get
+from sys import argv
+
+
+def main():
+    """function to prevent run if imported"""
+    url = f"https://api.github.com/repos/{argv[2]}/{argv[1]}/commits"
+    res = get(url)
+    data = res.json()[:10]
+    for i in range(10):
+        print(f"{data[i]['sha']}: {data[i]['commit']['author']['name']}")
+
+
+if __name__ == "__main__":
+    main()
